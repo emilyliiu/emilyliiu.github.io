0ab6c236929cf471e5e5d966137ddcc04411bb1d
Emily Liu <emilyliu107@gmail.com>
2019-07-01 22:22:29 -0700

    v2
    Cleaned css, fixed animation bug, logo update, uwmcc + yfp page update

--- index.js
+++ index.js
@@ -1,87 +1,83 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-	$.fn.animateRotate = function(angle, duration, easing, complete) {
-	  var args = $.speed(duration, easing, complete);
-	  var step = args.step;
-	  return this.each(function(i, e) {
-		args.complete = $.proxy(args.complete, e);
-		args.step = function(now) {
-		  $.style(e, 'transform', 'rotate(' + now + 'deg)');
-		  if (step) return step.apply(e, arguments);
-		};
+	$.fn.animateRotate = function (angle, duration, ease, complete) {
+		var args = $.speed(duration, ease, complete);
+		var step = args.step;
+		return this.each(function (i, e) {
+			args.complete = $.proxy(args.complete, e);
+			args.step = function (now) {
+				$.style(e, 'transform', 'rotate(' + now + 'deg)');
+				if (step) return step.apply(e, arguments);
+			};
 
-		$({deg: 0}).animate({deg: angle}, args);
-	  });
+			$({
+				deg: 0
+			}).animate({
+				deg: angle
+			}, args);
+		});
 	};
-	
+
 	$("#about").css("background-color", "#fff");
-	$("#about").css("height", "100vh");
+	$("#about").css("height", "100%");
 	$("#about").css("width", "100%");
 	$("#about").fadeIn();
 	$(".about-content").fadeIn();
-	
-	$(".about-animate").on("click", function() {
+
+	$(".about-animate").on("click", function () {
 		$(".about-content").fadeOut();
-		$("#about").animate({
-			width: "25px",
-			height: "375px"
-		}, function() {
-			$(this).animateRotate(90);
-		});
-		
-		setTimeout(function() {
-			$("#about").fadeOut();		 
-		}, 1500);
-		
-		setTimeout(function() {
+
+		setTimeout(function () {
+			$("#about").fadeOut();
+		}, 1000);
+
+		setTimeout(function () {
 			$("#more-about").animateRotate(0, 0);
-			$("#more-about").css("height", "25px");
-			$("#more-about").css("width", "375px");
 			$("#more-about").fadeIn();
 			$("#more-about").animate({
-				backgroundColor: "#4e3c7c"
-			}, function() {
+				backgroundColor: "#e4e4e4"
+			}, function () {
 				$(this).animate({
-					height: "100vh"
-				}, function() {
+					height: "100%"
+				}, function () {
 					$(this).animate({
 						width: "100%"
-					}, function() {
-						$(".more-content").fadeIn(300);
+					}, function () {
+						$(".more-content").fadeIn(500);
 					});
 				});
 			});
-		}, 800);
+		}, 1000);
 	});
-		
-	$(".more-animate").on("click", function() {
+
+	$(".more-animate").on("click", function () {
 		$(".more-content").fadeOut();
 		$("#more-about").animate({
 			width: "25px",
 			height: "375px"
-		}, function() {
+		}, function () {
 			$(this).animateRotate(-90);
 		});
-		
-		setTimeout(function() {
-			$("#more-about").fadeOut();			
-		}, 1500);
-		
-		setTimeout(function() {
-		$("#about").animateRotate(0, 0);
-		$("#about").css("height", "25px");
-		$("#about").css("width", "375px");
+
+		setTimeout(function () {
+			$("#more-about").fadeOut();
+		}, 1000);
+
+		setTimeout(function () {
+			$("#about").animateRotate(0, 0);
+			$("#about").css("height", "25px");
+			$("#about").css("width", "375px");
 			$("#about").fadeIn();
 			$("#about").animate({
 				height: "100vh"
-			}, function() {
+			}, function () {
 				$(this).animate({
 					width: "100%"
-				}, function() {
-					$(".about-content").fadeIn(300);
+				}, function () {
+					$(".about-content").fadeIn(1000);
 				});
 			});
-		}, 1400);
+		}, 1000);
 	});
-	
+
 });
