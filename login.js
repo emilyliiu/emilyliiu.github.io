a0e6c33e46fa9132e7fac0503d7d4f9b11c85e18
Emily Liu <emilyliu107@gmail.com>
2020-12-21 17:58:51 -0800

    Wish pswd protect

---
+++ login.js
@@ -0,0 +1,22 @@
+var attempt = 3;
+
+function validate() {
+    var password = document.getElementById("password").value;
+    if (password == "123") {
+        ;
+
+
+        window.location = "Wish/5FBiXSAOsDYyll0JvhCeSAU.html";
+        return false;
+    } else {
+        attempt--; 
+        document.getElementById("errorMessage").textContent="Incorrect username and/or password";
+
+        if (attempt == 0) {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
